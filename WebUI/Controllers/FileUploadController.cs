15bfd87737db3eb65494df66ebfaaaaab492e62c
Taz Uddin <getleton@gmail.com>
2020-02-21 22:59:54 +0600

    some clean up

---
+++ WebUI/Controllers/FileUploadController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        
+    }
+}
